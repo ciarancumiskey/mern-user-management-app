f603a34079610a7cd79d7e3d9e81fc8f854445fd
Ciaran Cumiskey <ciarancumiskey18594@gmail.com>
2021-04-06 14:50:44 +0100

    Created schema for User data

---
+++ server/models/user.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema(
+    {
+        first_name: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        surname: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        user_email: {
+            type: String,
+            required: true,
+            trim: true,
+            validate(value) {
+                if (!value.match(/^[^@ ]+@[^@ ]+\.[^@ .]{2,}$/)) {
+                    throw new Error('Please enter a correctly-formatted email address.');
+                }
+            }
+        },
+        user_password: {
+            type: String,
+            required: true,
+            trim: true,
+            minlength: 6
+        },
+        country: {
+            type: String,
+            required: true,
+            trim: true
+        },
+        state: {
+            type: String,
+            trim: true
+        },
+        city: {
+            type: String,
+            trim: true
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+const User = mongoose.model('User', userSchama);
+
+module.exports = User;
