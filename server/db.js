9ff20a8c61a36ccf27a8a0143307136dd586c513
Ciaran Cumiskey <ciarancumiskey18594@gmail.com>
2021-04-07 11:00:09 +0100

    Added login functionality to db.js

--- server/db.js
+++ server/db.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 
+//Connects to the "mern-user-db" database in my Mongo instance
 mongoose.connect('mongodb://127.0.0.1:27017/mern-user-db', {
     useNewUrlParser: true,
     useCreateIndex: true,
