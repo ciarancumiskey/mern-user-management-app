5873751c42c99bf984d2f5f4ad90558e83cefd48
Ciaran Cumiskey <ciarancumiskey18594@gmail.com>
2021-04-07 12:04:55 +0100

    Removed CORS

--- server/index.js
+++ server/index.js
@@ -1,17 +1,22 @@
 const userRouter = require('./routers/user');
-const cors = require('cors');
+const path = require('path');
 const express = require('express');
 require('./db');
 
 const app = express();
 const PORT = process.env.PORT || 3030;
 
+//Configure ExpressJS so the /build folder is used as the starting point of the app
+app.use(express.static(path.join(__dirname, '..', 'build')));
 app.use(express.json());
-app.use(cors());
 app.use(userRouter);
 
 app.get('/', (request, result) => {
     result.send('<h2>This is from index.js</h2>');
+});
+
+app.use((req, res, next) => {
+    res.sendFile(path.join(__dirname, '..', 'build', 'index.html'));
 });
 
 app.listen(PORT, () => {
